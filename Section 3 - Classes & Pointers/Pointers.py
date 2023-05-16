34029a1db335a4c448c08ccc011bf36b14638789
Joel <salas.22@buckeyemail.osu.edu>
2023-05-16 16:00:53 -0400

    Update Pointers.py

--- Section 3 - Classes & Pointers/Pointers.py
+++ Section 3 - Classes & Pointers/Pointers.py
@@ -1,1 +1,77 @@
+num1 = 11
 
+num2 = num1
+
+print("Before num2 value is updated:")
+print("num1 =", num1)
+print("num2 =", num2)
+
+print("\nnum1 points to:", id(num1))
+print("num2 points to:", id(num2)) 
+
+num2 = 22 
+
+print("\nAfter num2 value is updated:")
+print("num1 =", num1)
+print("num2 =", num2) 
+
+print("\nnum1 points to:", id(num1))
+print("num2 points to:", id(num2))
+
+#####################################
+
+
+dict1 = {
+         'value': 11
+        }
+
+dict2 = dict1 
+
+print("\n\nBefore value is updated:")
+print("dict1 =", dict1)
+print("dict2 =", dict2)
+
+print("\ndict1 points to:", id(dict1))
+print("dict2 points to:", id(dict2)) 
+
+dict2['value'] = 22
+
+print("\nAfter value is updated:")
+print("dict1 =", dict1)
+print("dict2 =", dict2) 
+
+print("\ndict1 points to:", id(dict1))
+print("dict2 points to:", id(dict2))
+# ------------OutPut------------
+"""
+
+Before num2 value is updated:
+num1 = 11
+num2 = 11
+
+num1 points to: 4394051048
+num2 points to: 4394051048
+
+After num2 value is updated:
+num1 = 11
+num2 = 22
+
+num1 points to: 4394051048
+num2 points to: 4394051400
+
+
+Before value is updated:
+dict1 = {'value': 11}
+dict2 = {'value': 11}
+
+dict1 points to: 4375957760
+dict2 points to: 4375957760
+
+After value is updated:
+dict1 = {'value': 22}
+dict2 = {'value': 22}
+
+dict1 points to: 4375957760
+dict2 points to: 4375957760
+
+"""
