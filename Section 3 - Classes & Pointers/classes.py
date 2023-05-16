2a18a20f52a00210deb565b0119e5cd374140e72
Joel <salas.22@buckeyemail.osu.edu>
2023-05-16 15:58:32 -0400

    Update classes.py

--- Section 3 - Classes & Pointers/classes.py
+++ Section 3 - Classes & Pointers/classes.py
@@ -1,1 +1,27 @@
+# Our Constructor, starts out looking like a function but what's different 
+# about a method, then a function is the 'self' keyword 
 
+class Cookie:
+    def __init__(self, color): 
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+    def set_color(self, color):
+        self.color = color
+
+
+cookie_one = Cookie('green')
+cookie_two = Cookie('blue')
+
+print('Cookie one is', cookie_one.get_color())
+print('Cookie two is', cookie_two.get_color())
+
+cookie_one.set_color('yellow')
+
+print('\nCookie one is now', cookie_one.get_color())
+print('Cookie two is still', cookie_two.get_color())
+
+"""
+The difference 
