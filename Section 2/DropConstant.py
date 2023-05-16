31b34c71d5dab1dccd7154d61b600fee0ef64068
Joel <salas.22@buckeyemail.osu.edu>
2023-05-16 15:37:54 -0400

    Create DropConstant.py

---
+++ Section 2/DropConstant.py
@@ -0,0 +1,31 @@
+def print_items(n):
+    for i in range(n):
+        print(i)
+
+    for j in range(n):
+        print(j)
+
+print_items(10)
+--Output--
+O(2n) --> O(n)
+
+0
+1
+2
+3
+4
+5
+6
+7
+8
+--
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
