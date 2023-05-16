b9298bf53371974474ea9a792be0bfb9cd4c74c1
Joel <salas.22@buckeyemail.osu.edu>
2023-05-16 15:40:03 -0400

    Update O(1).py

--- Section 2/O(1).py
+++ Section 2/O(1).py
@@ -1,1 +1,9 @@
-
+def add_items(n):
+    return n + n
+  
+  -------------
+  def add_items(n):
+    return n + n + n
+  
+  
+# Still O(1) aka Constant Time, as N increases, the number of operations is going to remain constant.
