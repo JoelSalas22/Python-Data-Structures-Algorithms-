d1ad7964df1c942a1f9778f02260b9108c2bdfc1
Joel <salas.22@buckeyemail.osu.edu>
2023-05-16 16:18:17 -0400

    Update LL-Append.py
    A method to append at the end of Linked List

--- Section 4: Linked Lists/LL-Append.py
+++ Section 4: Linked Lists/LL-Append.py
@@ -1,1 +1,48 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
 
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+            
+    def make_empty(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = self.head
+        else:
+            self.tail.next = new_node
+            self.tail = self.tail.next
+        self.length += 1
+        return True
+      
+      
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Head: 1
+    Tail: 2
+    Length: 2 
+
+    Linked List:
+    1
+    2
+    
+"""
